e1d5c5af42b95cdfc70a46eff92c969b40091343
Azzarrel <t.hofmann93@gmail.com>
2020-09-07 21:57:03 +0200

    BattleMap added

--- Pow.BattleMapper/MainWindow.xaml.cs
+++ Pow.BattleMapper/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PoW.BattleMap
+namespace Pow.BattleMapper
 {
   /// <summary>
-  /// Interaction logic for BattleMapView.xaml
+  /// Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class BattleMapSelector : UserControl
+  public partial class MainWindow : Window
   {
-    public BattleMapSelector()
+    public MainWindow()
     {
       InitializeComponent();
     }
